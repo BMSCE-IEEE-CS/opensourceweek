6fe8d16114051df2bf420c472f53de13730e3211
Vaibhav-Reddy560 <vaibhav.reddy560@gmail.com>
2025-11-09 08:49:01 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,8 +16,8 @@ export default function Navbar() {
               <Image 
                 src="/club-logo.png" 
                 alt="BMSCE IEEE CS Logo" 
-                width={85} 
-                height={85} 
+                width={100} 
+                height={100} 
               />
               {/* 2. Removed the span text */}
             </div>
