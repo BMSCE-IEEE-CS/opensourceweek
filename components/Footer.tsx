f87c9b1e97b4b95fad0aefe6cc0518f0d0d473e0
Vaibhav-Reddy560 <vaibhav.reddy560@gmail.com>
2025-11-09 10:51:10 +0530

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -111,7 +111,7 @@ export default function Footer() {
             {/* This is the real, working embed link */}
             <iframe
               title="BMSCE Location"
-              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.487818408272!2d77.56372877590827!3d12.94060888736932!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae158b11e445f3%3A0x10126b1c8f36b6f3!2sBMS%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1730871952391!5m2!1sen!2sin"
+              src="maps.google.com/8"
               className="w-full h-full"
               style={{ border: 0 }}
               allowFullScreen={true}
